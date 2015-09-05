bb5816e0b549207ea9e499fa2c2380d09ed16acd
Tony Beltramelli <tbeltramelli@gmail.com>
2015-09-05 18:40:10 +0200

    implement training web mobile app

--- client/trainer/assets/js/main.js
+++ client/trainer/assets/js/main.js
@@ -1,6 +1,60 @@
 $(document).ready(function(){main();});
 
+var LOCAL_SERVER_ADDRESS = "swat:serverAddress";
+var LOCAL_SERVER_PORT = "swat:serverPort";
+
+var serverAddress;
+var serverPort;
+
 function main ()
 {
-	
+	$("main").hide();
+
+	serverAddress = localStorage.getItem(LOCAL_SERVER_ADDRESS);
+	serverPort = localStorage.getItem(LOCAL_SERVER_PORT);
+
+	if(serverAddress) $("#serverAddress").val(serverAddress);
+	if(serverPort) $("#serverPort").val(serverPort);
+
+	$("#saveButton").click(function(e){
+		serverAddress = $("#serverAddress").val();
+		serverPort = $("#serverPort").val();
+
+		localStorage.setItem(LOCAL_SERVER_ADDRESS, serverAddress);
+		localStorage.setItem(LOCAL_SERVER_PORT, serverPort);
+
+		$(this).parent().hide();
+		$("main").show();
+	});
+
+	$(".keypad li").on("mousedown", function(e){
+		timestamp = Date.now();
+
+		e.stopPropagation();
+		e.preventDefault();
+
+		$(this).css({
+			background: "#1caaff",
+			color: "#fcfcfc"
+		});
+
+		var label = $(this).find("span").text();
+		send(timestamp, label);
+	});
+
+	$(".keypad li").on("mouseup", function(e){
+		$(this).css({
+			background: "#fcfcfc",
+			color: "#1caaff"
+		});
+	});
+}
+
+function send(timestamp, label)
+{
+	$.ajax({
+		url: serverAddress+":"+serverPort,
+		type: "POST",
+		data: "{timestamp: "+timestamp+", label: "+label+"}"
+	});
 }
--- server/socket/src/com/tonybeltramelli/swat/server/Main.java
+++ server/socket/src/com/tonybeltramelli/swat/server/Main.java
@@ -1,9 +1,15 @@
 package com.tonybeltramelli.swat.server;
 
-import com.tonybeltramelli.swat.server.network.Server;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.net.InetAddress;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
 
 /**
  * @author Tony Beltramelli www.tonybeltramelli.com - created 23/07/15
@@ -12,6 +18,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException
     {
+        /*
         Server server = new Server(Const.SERVER_PORT);
 
         System.out.println(InetAddress.getLocalHost().getHostAddress());
@@ -23,6 +30,27 @@ public class Main {
             }
         } finally {
             server.close();
+        }*/
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new MyHandler());
+        server.setExecutor(Executors.newCachedThreadPool());
+        server.start();
+    }
+
+    static class MyHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+            BufferedReader input = new BufferedReader(new InputStreamReader(t.getRequestBody()));
+            String data = input.readLine();
+
+            System.out.println("data: " + data);
+
+            String response = "200 (Success)\n";
+            t.sendResponseHeaders(200, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
         }
     }
 }
