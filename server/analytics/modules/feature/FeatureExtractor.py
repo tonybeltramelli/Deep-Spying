b5f055ca56c3ff17d5679b1cc1e83e6ccc87b08b
Tony Beltramelli <tbeltramelli@gmail.com>
2015-09-15 18:21:21 +0200

    add figure folder

--- server/analytics/modules/feature/FeatureExtractor.py
+++ server/analytics/modules/feature/FeatureExtractor.py
@@ -23,11 +23,6 @@ class FeatureExtractor:
         output_file = open("{}labelled.data".format(self.output_path), 'w')
         segments = []
 
-        import pylab
-        f, axes = pylab.subplots(2, 4, sharex='col', sharey='row')
-
-        axes = [axes[0, 0], axes[0, 1], axes[0, 2], axes[0, 3], axes[1, 0], axes[1, 1], axes[1, 2], axes[1, 3]]
-
         for i in range(0, len(label_timestamps)):
             center_timestamp_index = (np.abs(sensor.timestamp - label_timestamps[i])).argmin()
 
@@ -37,11 +32,6 @@ class FeatureExtractor:
             x_sample = self.get_data_slice(sensor.x, center_timestamp_index)
             y_sample = self.get_data_slice(sensor.y, center_timestamp_index)
             z_sample = self.get_data_slice(sensor.z, center_timestamp_index)
-
-            axes[i].plot(x_sample, color='r', label='x')
-            axes[i].plot(y_sample, color='g', label='y')
-            axes[i].plot(z_sample, color='b', label='z')
-            axes[i].set_title("key {}".format(labels[i]))
 
             output_file.write("label:{}\n".format(labels[i]))
 
@@ -55,8 +45,6 @@ class FeatureExtractor:
 
             output_file.write("\n")
         output_file.close()
-
-        self.view.save("{}{}_unique_segments.png".format(Path.FIGURE_PATH, sensor.id))
 
         print "Save features in {}".format(output_file.name)
 
