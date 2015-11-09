c6332c57756c225298a672c33aacc8f181056b9a
Tony Beltramelli <tbeltramelli@gmail.com>
2015-11-09 15:52:54 +0100

    update current architecture

---
+++ server/analytics/modules/utils/UNeuralNet.py
@@ -0,0 +1,48 @@
+__author__ = 'Tony Beltramelli www.tonybeltramelli.com - 17/10/2015'
+
+from pybrain.structure import *
+
+
+class UNeuralNet:
+
+    @staticmethod
+    def get_neural_net(input_number, output_number, NetworkType, HiddenLayerType, neurons_per_layer=[9], use_bias=False):
+        input = LinearLayer(input_number)
+        output = SoftmaxLayer(output_number)
+
+        neural_net = NetworkType()
+        neural_net.addInputModule(input)
+        neural_net.addOutputModule(output)
+
+        if use_bias:
+            bias = BiasUnit()
+            neural_net.addModule(bias)
+
+        prev = input
+        for i in range(0, len(neurons_per_layer)):
+            hidden = HiddenLayerType(neurons_per_layer[i])
+
+            neural_net.addModule(hidden)
+            neural_net.addConnection(FullConnection(prev, hidden))
+
+            if use_bias:
+                neural_net.addConnection(FullConnection(bias, hidden))
+
+            prev = hidden
+
+        neural_net.addConnection(FullConnection(prev, output))
+
+        if use_bias:
+            neural_net.addConnection(FullConnection(bias, output))
+
+        neural_net.sortModules()
+
+        fast_net = neural_net.convertToFastNetwork()
+
+        if fast_net is not None:
+            neural_net = fast_net
+            print "Use fast C++ implementation"
+        else:
+            print "Use standard Python implementation"
+
+        return neural_net
