2a152c7881f12fcb7fae0892a0c37df9cd509c81
Tony Beltramelli <tbeltramelli@gmail.com>
2015-11-11 14:01:04 +0100

    temp

--- server/analytics/advanced/main.lua
+++ server/analytics/advanced/main.lua
@@ -3,10 +3,29 @@ require 'nn'
 require 'nngraph'
 require 'lfs'
 
-GPU_ID = 1
-USE_OPENCL = 0
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text("Options:")
+cmd:option("-data_path", "../../data/feature/", "pre-processed data directory")
+cmd:option("-labels", "1,2,3,4,5,6,7,8,9,0,*,#", "list of labels separated with a comma")
+cmd:option("-model", "lstm", "lstm | fnn | gru | rnn")
+cmd:option("-layer_size", 128, "size of hidden internal state")
+cmd:option("-layer_num", 2, "number of hidden layers")
+cmd:option("-max_iteration", 100, "maximum number of training iteration")
+cmd:option("-learning_rate", 2e-3, "learning rate")
+cmd:option("-learning_rate_decay", 0.97, "learning rate decay")
+cmd:option("-learning_rate_decay_after", 10, "in number of epochs, when to start decaying the learning rate")
+cmd:option("-decay_rate", 0.95, "decay rate for rmsprop")
+cmd:option("-dropout", 0, "dropout for regularization, used after each RNN hidden layer. 0 = no dropout")
+cmd:option("-init_weight", 0.08, "value interval to init weights")
+cmd:option("-seed", 123, "seed to pseudo-random number generator")
+cmd:option("-gpu", 0, "use gpu (OpenCL)")
+cmd:text()
+opt = cmd:parse(arg)
 
-if GPU_ID >= 0 and USE_OPENCL == 1 then
+GPU_ID = 1
+
+if opt.gpu == 1 then
     local n, clnn = pcall(require, 'clnn')
     local t, cltorch = pcall(require, 'cltorch')
 
@@ -18,157 +37,87 @@ else
 	print("Use CPU")
 end
 
-LABELS = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "*", "#"}
-HIDDEN_LAYERS = {256, 256}
-DATA_PATH = "../../data/feature/"
-INIT_WEIGHT=0.08
+require 'model.OneHot'
 
-metaData = nil
+local dataLoader = require 'loader.DataLoader'
+local lstm = require 'model.LSTM'
 
-function makeBinaryClasses(labelSet)
-	classes = {}
-	local len = #labelSet
-
-	for i = 1, len do
-		local t = torch.Tensor(1, len)
-		local binClasses = t:storage()
-
-		binClasses[i] = 1
-		classes[labelSet[i]] = torch.Tensor(binClasses)
-	end
+function cloneList(tensorList, toZero)
+    local out = {}
+    for k,v in pairs(tensorList) do
+        out[k] = v:clone()
+        if toZero then out[k]:zero() end
+    end
+    return out
 end
 
-function createDataset()
-	dataset = {}
-	local i = 0
-
-	for file in lfs.dir(DATA_PATH) do
-	    if string.match(file, ".data") then
-	    	local data = io.open(DATA_PATH..file, "r")
-
-	    	if data then
-	    		local outputVect = nil
-
-	    		for line in data:lines() do
-	    			if string.match(line, ":") then
-	    				local s, e = string.find(line, ":")
-		                local label = string.sub(line, s + 1)
-		                outputVect = classes[label]
-		            elseif string.match(line, ".") then
-		            	dataPoints = line:split(",")
-		            	
-		            	local len = #dataPoints
-		            	local t = torch.Tensor(1, len)
-		            	local inputVect = t:storage()
-
-		            	for j = 1, len do
-		            		inputVect[j] = dataPoints[j]
-		            	end
-
-		            	i = i + 1
-		               	dataset[i] = {torch.Tensor(inputVect), outputVect}
-
-		            	if metaData == nil then
-		            		metaData = {inputSize = len, outputSize = #LABELS}
-		            	end
-		            end
-				end
-			end
-
-	    	data.close()
-	    end
-	end
-	function dataset:size() return #dataset end
-end
-
-function buildNeuralNet()
-	-- torch.manualSeed(1234)
-
-	-- local LSTM = require 'model.LSTM'
-	-- model = LSTM.model(metaData.inputSize, metaData.outputSize, HIDDEN_LAYERS, INIT_WEIGHT)
-
-	inputSize = 2
-	outputSize = 4
-	layerSize = 10
-	initWeight = 0.08
-	layers = 2
-
-	local inputs = {}
-	local outputs = {}
-
-	table.insert(inputs, nn.Identity()())
-	for i = 1, layers do
-		table.insert(inputs, nn.Identity()())
-		table.insert(inputs, nn.Identity()())
-	end
-
-	local input = inputs[1]
-
-	for i = 1, layers do
-		local prevC = inputs[i * 2]
-		local prevY = inputs[i * 2 + 1]
-
-		local function lstmCell(input, prevY, prevC)
-	    	local xi = nn.Linear(inputSize, layerSize)(input)
-		    local prevYi = nn.Linear(layerSize, layerSize)(prevY)
-		    local gateInput = nn.CAddTable()({xi, prevYi})
-		    
-		    local inputGate = nn.Sigmoid()(gateInput)
-		    local forgetGate = nn.Sigmoid()(gateInput)
-		    local cellInput = nn.Tanh()(gateInput)
-		    local nextC = nn.CAddTable()({
-		      nn.CMulTable()({forgetGate, prevC}),
-		      nn.CMulTable()({inputGate, cellInput})
-		    })
-		    local outputGate = nn.Sigmoid()(gateInput)
-		    local nextY = nn.CMulTable()({outputGate, nn.Tanh()(nextC)})
-		    return nextY, nextC
-		end
-
-		local nextY, nextC = lstmCell(input, prevY, prevC)
-		table.insert(outputs, nextC)
-		table.insert(outputs, nextY)
-	end
-	
-	local topY = outputs[#outputs]
-	local prediction = nn.LogSoftMax()(nn.Linear(layerSize, outputSize)(topY))
-	table.insert(outputs, prediction)
-
-	model = nn.gModule(inputs, outputs)
-	model:getParameters():uniform(-initWeight, initWeight)
-end
-
-function train()
+function trainAutomatic()
 	-- lossFunction = nn.ClassNLLCriterion()
 	local lossFunction = nn.MSECriterion()
 
 	trainer = nn.StochasticGradient(model, lossFunction)
-	trainer.learningRate = 0.001
-	trainer.learningRateDecay = 0.9
-	trainer.maxIteration = 100
+	trainer.learningRate = opt.learning_rate
+	trainer.learningRateDecay = opt.learning_rate_decay
+	trainer.maxIteration = opt.max_iteration
 	trainer.shuffleIndices = false
 	trainer:train(dataset)
 end
 
---makeBinaryClasses(LABELS)
---createDataset()
---buildNeuralNet()
---train()
+function train(model, dataset, criterion, initWeight)
+	model:getParameters():uniform(-initWeight, initWeight)
+	local states = {}
+	local initialState = {}
+	local predictions = {}
+	local loss = 0
 
+	for i = 1, opt.max_iteration do
+		if i == 1 then
+			for i = 1, opt.layer_num do
+			    local emptyVector = torch.zeros(1, opt.layer_size)
+			    table.insert(initialState, emptyVector:clone())
+			    table.insert(initialState, emptyVector:clone())
+			end
+
+			states = {[0] = cloneList(initialState)}
+		end
+
+		local inputVector = dataset[i][1]
+		local expectedOutputVector = dataset[i][2] 
+
+		local out = model:forward({inputVector, unpack(states[i - 1])})
+
+		states[i] = {}
+		for i=1, #out - 1 do
+			table.insert(states[i], out[i])
+		end
+
+		predictions[i] = out[#out]
+		loss = loss + criterion:forward(predictions[i], expectedOutputVector)
+	end
+
+	loss = loss / opt.max_iteration
+
+	local backwardStates = {[opt.max_iteration] = cloneList(initialState, true)}
+
+	for i = opt.max_iteration, 1, -1 do
+		local backpropagated = criterion:backward(predictions[i], expectedOutputVector)
+		table.insert(backwardStates[i], backpropagated)
+
+		local derivative = model:backward({inputVector, unpack(states[i - 1])}, backwardStates[i])
+		backwardStates[t - 1] = {}
+
+		for k, v in pairs(derivative) do
+            if k > 1 then
+            	backwardStates[t-1][k-1] = v
+            end
+        end
+	end
+end
+
+dataLoader.load(opt.data_path, opt.labels:split(","))
+local dataset = dataLoader.dataset
+local model = lstm.get(dataLoader.metaData.inputSize, dataLoader.metaData.outputSize, opt.layer_size, opt.layer_num)
+local lossFunction = nn.MSECriterion()
+
+train(model, dataset, lossFunction, opt.init_weight)
 -- os.exit()
-
-
-
-
-
-
-
-buildNeuralNet()
-
-local t = torch.Tensor(1, 2)
-local inputVect = t:storage()
-inputVect[1] = 0.1
-inputVect[2] = 0.2
-
-out = model:forward({torch.Tensor(inputVect), torch.randn(1, 10), torch.randn(1, 10), torch.randn(1, 10), torch.randn(1, 10)})
-print(out[#out])
